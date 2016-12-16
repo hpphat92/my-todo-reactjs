3aac5330009298c4774be986e5a1b4dd7660c7bc
andy <andy@d8ii.com>
2016-12-16 12:31:09 +0700

    Init code

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: './main.js',
+
+  output: {
+    path: './',
+    filename: 'index.js',
+  },
+
+  devServer: {
+    inline: true,
+    port: 8080
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+}
