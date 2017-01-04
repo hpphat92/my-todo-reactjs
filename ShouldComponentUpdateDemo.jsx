26b4d913685da08a03678419da30678b2d5d5a62
andy <andy@d8ii.com>
2017-01-04 15:31:18 +0700

    Fix css

--- ShouldComponentUpdateDemo.jsx
+++ ShouldComponentUpdateDemo.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-function log(msg) {
-  document.querySelector('#console').innerText += `\n${msg}.`;
+function log(msg, id) {
+  document.querySelector('#console').innerHTML += `<br><div class="msg ${id}">${msg}</div>.`;
 }
 function clear() {
-  document.querySelector('#console').innerText = "";
+  document.querySelector('#console').innerHTML = "";
 }
 class ParentComp extends React.Component {
   constructor(props) {
@@ -15,11 +15,12 @@ class ParentComp extends React.Component {
 
   set(val) {
     this.setState({x: val}, () => {
-      log(`Changed State of parent component to x=${this.state.x}`);
+      log(`Changed State of parent component to x=${this.state.x}`, 'parent');
     })
   }
 
   shouldComponentUpdate() {
+    log(`Check should component update for parent component and return ${this.state.shouldComponentUpdatedBoolean}`,'parent');
     return this.state.shouldComponentUpdatedBoolean;
   }
 
@@ -32,11 +33,11 @@ class ParentComp extends React.Component {
   }
 
   componentDidUpdate() {
-    log('Parent Component Updated');
+    log('Parent Component Updated', 'parent');
   }
 
   componentWillUpdate() {
-    log('Parent Component Will Be Updated');
+    log('Parent Component Will Be Updated', 'parent');
   }
 
   render() {
@@ -48,26 +49,44 @@ class ParentComp extends React.Component {
         <button onClick={() => this.set(1)}>Change state x=1</button>
         <button onClick={() => this.set(3)}>Change state x=3</button>
         <button onClick={() => this.forceUpdate()}>Force Update</button>
-        <input class="block" type="checkbox" value="this.state.shouldComponentUpdatedBoolean" onChange={() => this.updateStatus()}/> Component need updated
+        <div>
+
+          <input type="checkbox" value="this.state.shouldComponentUpdatedBoolean"
+                 onChange={() => this.updateStatus()}/> Component need updated
+        </div>
       </div>
     )
   }
 }
 
 class NoProps extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      x: 3
+    }
+  }
+
+  set(val) {
+    this.setState({x: val});
+  }
 
   render() {
     return (
-      <div className="child-no-props">Component without props passed</div>
+      <div className="child-no-props">
+        Component without props passed (local state) x: {this.state.x}
+        <button onClick={() => this.set(1)}>Change state x=1</button>
+      </div>
     )
   }
 
   shouldComponentUpdate() {
-    log('Check should component update for Component without props passed and return true');
+    log('Check should component update for Component without props passed and return true', 'no-prop');
     return true;
   }
+
   componentDidUpdate() {
-    log('Component without props passed re-renderd');
+    log('Component without props passed re-rendered', 'no-prop');
   }
 }
 class PropComponent extends React.Component {
@@ -79,15 +98,16 @@ class PropComponent extends React.Component {
   }
 
   shouldComponentUpdate() {
-    log('Check should component update for Component with props passed and return true');
+    log('Check should component update for Component with props passed and return true', 'has-prop');
     return true;
   }
+
   componentDidUpdate() {
-    log(`Component with props passed rendered with next props: ${this.props.v}`)
+    log(`Component with props passed rendered with next props: ${this.props.v}`, 'has-prop')
   }
 
   componentWillUpdate() {
-    log(`Component with props passed will be rendered with next props: ${this.props.v}`)
+    log(`Component with props passed will be rendered with next props: ${this.props.v}`, 'has-prop')
   }
 }
 
@@ -106,7 +126,7 @@ class ShouldComponentUpdateDemo extends React.Component {
       <div className="should-component-update-demo">
         <ParentComp/>
         <div className="console">
-          Console: <span class="pointer" onClick={() => this.clear()}>[X]</span>
+          Console: <span className="pointer" onClick={() => this.clear()}>[X]</span>
           <div id="console"></div>
         </div>
       </div>
